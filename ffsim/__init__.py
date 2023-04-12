89a5840b605f97efb88d01a1b586fdde5c31d13d
Kevin J. Sung <kevjsung@umich.edu>
2023-04-12 13:36:35 -0400

    export functions in __init__.py

--- ffsim/__init__.py
+++ ffsim/__init__.py
@@ -7,3 +7,29 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+
+"""ffsim is a software library for fast simulation of fermionic quantum circuits."""
+
+from ffsim.double_factorized import (
+    double_factorized_decomposition,
+)
+
+from ffsim.gates import (
+    apply_diag_coulomb_evolution,
+    apply_givens_rotation,
+    apply_num_interaction,
+    apply_num_op_prod_interaction,
+    apply_num_op_sum_evolution,
+    apply_orbital_rotation,
+    apply_phase_shift,
+    apply_tunneling_interaction,
+)
+
+from ffsim.states import (
+    one_hot,
+    slater_determinant,
+)
+
+from ffsim.trotter import (
+    simulate_trotter_suzuki_double_factorized,
+)
