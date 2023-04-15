75c99b701f348dc6901b738d5cc9a56599312de8
Kevin J. Sung <kevjsung@umich.edu>
2023-04-14 21:17:59 -0400

    test diag coulomb with mat_alpha_beta

--- ffsim/gates_test.py
+++ ffsim/gates_test.py
@@ -207,15 +207,49 @@ def test_apply_diag_coulomb_evolution():
         state = slater_determinant(norb, occupied_orbitals)
         original_state = state.copy()
 
-        core_tensor = np.real(np.array(random_hermitian(norb, seed=rng)))
+        mat = np.real(np.array(random_hermitian(norb, seed=rng)))
         time = 0.6
-        result = apply_diag_coulomb_evolution(core_tensor, state, time, norb, nelec)
+        result = apply_diag_coulomb_evolution(mat, state, time, norb, nelec)
 
         eig = 0
         for i, j in itertools.product(range(norb), repeat=2):
             for sigma, tau in itertools.product(range(2), repeat=2):
                 if i in occupied_orbitals[sigma] and j in occupied_orbitals[tau]:
-                    eig += 0.5 * core_tensor[i, j]
+                    eig += 0.5 * mat[i, j]
+        expected = np.exp(-1j * eig * time) * state
+
+        np.testing.assert_allclose(result, expected, atol=1e-8)
+        np.testing.assert_allclose(state, original_state)
+
+
+def test_apply_diag_coulomb_evolution_alpha_beta():
+    """Test applying time evolution of diagonal Coulomb operator with alpha beta mat."""
+    norb = 5
+    for _ in range(5):
+        rng = np.random.default_rng()
+        n_alpha = rng.integers(1, norb + 1)
+        n_beta = rng.integers(1, norb + 1)
+        occupied_orbitals = (
+            rng.choice(norb, n_alpha, replace=False),
+            rng.choice(norb, n_beta, replace=False),
+        )
+        nelec = tuple(len(orbs) for orbs in occupied_orbitals)
+        state = slater_determinant(norb, occupied_orbitals)
+        original_state = state.copy()
+
+        mat = np.real(np.array(random_hermitian(norb, seed=rng)))
+        mat_alpha_beta = np.real(np.array(random_hermitian(norb, seed=rng)))
+        time = 0.6
+        result = apply_diag_coulomb_evolution(
+            mat, state, time, norb, nelec, mat_alpha_beta=mat_alpha_beta
+        )
+
+        eig = 0
+        for i, j in itertools.product(range(norb), repeat=2):
+            for sigma, tau in itertools.product(range(2), repeat=2):
+                if i in occupied_orbitals[sigma] and j in occupied_orbitals[tau]:
+                    this_mat = mat if sigma == tau else mat_alpha_beta
+                    eig += 0.5 * this_mat[i, j]
         expected = np.exp(-1j * eig * time) * state
 
         np.testing.assert_allclose(result, expected, atol=1e-8)
