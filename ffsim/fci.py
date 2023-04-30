2f2f13b6af26883f5e57604e60c89fd5e9d2adaa
Kevin J. Sung <kevjsung@umich.edu>
2023-04-30 19:59:48 -0400

    port contract2e from pyscf and use cached link index

--- ffsim/fci.py
+++ ffsim/fci.py
@@ -14,9 +14,9 @@ import itertools
 
 import numpy as np
 import scipy.sparse.linalg
+from pyscf import lib
 from pyscf.fci import cistring
 from pyscf.fci.direct_nosym import absorb_h1e, contract_1e, make_hdiag
-from pyscf.fci.fci_slow import contract_2e
 from scipy.special import comb
 
 from ffsim._ffsim import (
@@ -24,6 +24,43 @@ from ffsim._ffsim import (
     contract_diag_coulomb_into_buffer,
     contract_num_op_sum_spin_into_buffer,
 )
+
+
+def contract_2e(eri, fcivec, norb, nelec, link_index=None):
+    # source: pyscf
+    # modified to accept cached link index
+    """Compute E_{pq}E_{rs}|CI>"""
+    if isinstance(nelec, (int, np.integer)):
+        nelecb = nelec // 2
+        neleca = nelec - nelecb
+    else:
+        neleca, nelecb = nelec
+    if link_index is None:
+        link_indexa = cistring.gen_linkstr_index(range(norb), neleca)
+        link_indexb = cistring.gen_linkstr_index(range(norb), nelecb)
+    else:
+        link_indexa, link_indexb = link_index
+    na = cistring.num_strings(norb, neleca)
+    nb = cistring.num_strings(norb, nelecb)
+    ci0 = fcivec.reshape(na, nb)
+    t1 = np.zeros((norb, norb, na, nb), dtype=fcivec.dtype)
+    for str0, tab in enumerate(link_indexa):
+        for a, i, str1, sign in tab:
+            t1[a, i, str1] += sign * ci0[str0]
+    for str0, tab in enumerate(link_indexb):
+        for a, i, str1, sign in tab:
+            t1[a, i, :, str1] += sign * ci0[:, str0]
+
+    t1 = lib.einsum("bjai,aiAB->bjAB", eri.reshape([norb] * 4), t1)
+
+    fcinew = np.zeros_like(ci0, dtype=fcivec.dtype)
+    for str0, tab in enumerate(link_indexa):
+        for a, i, str1, sign in tab:
+            fcinew[str1] += sign * t1[a, i, str0]
+    for str0, tab in enumerate(link_indexb):
+        for a, i, str1, sign in tab:
+            fcinew[:, str1] += sign * t1[a, i, :, str0]
+    return fcinew.reshape(fcivec.shape)
 
 
 def get_dimension(norb: int, nelec: tuple[int, int]) -> int:
@@ -41,12 +78,15 @@ def get_hamiltonian_linop(
     nelec: tuple[int, int],
 ) -> scipy.sparse.linalg.LinearOperator:
     """Get the Hamiltonian in the FCI basis."""
-    # TODO use cached link_indexa and link_indexb
+    n_alpha, n_beta = nelec
+    linkstr_index_a = cistring.gen_linkstr_index(range(norb), n_alpha)
+    linkstr_index_b = cistring.gen_linkstr_index(range(norb), n_beta)
+    link_index = (linkstr_index_a, linkstr_index_b)
     two_body = absorb_h1e(one_body_tensor, two_body_tensor, norb, nelec, 0.5)
     dim = get_dimension(norb, nelec)
 
     def matvec(vec: np.ndarray):
-        return contract_2e(two_body, vec, norb, nelec)
+        return contract_2e(two_body, vec, norb, nelec, link_index=link_index)
 
     return scipy.sparse.linalg.LinearOperator(
         shape=(dim, dim), matvec=matvec, rmatvec=matvec, dtype=one_body_tensor.dtype
