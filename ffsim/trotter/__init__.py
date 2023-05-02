d25de38ff6d5ef4cd3137a92506a07939dcfbee9
Kevin J. Sung <kevjsung@umich.edu>
2023-05-02 16:07:45 -0400

    create trotter module

---
+++ ffsim/trotter/__init__.py
@@ -0,0 +1,13 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Hamiltonian simulation via Trotter-Suzuki formulas."""
+
+from ffsim.trotter.trotter import simulate_trotter_double_factorized
