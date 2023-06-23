a1d28565d73fb5647d4901419e0aa468b3d87f6e
Kevin J. Sung <kevjsung@umich.edu>
2023-06-23 13:21:43 -0400

    add sphinx config

---
+++ docs/conf.py
@@ -0,0 +1,48 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Sphinx configuration."""
+
+import importlib.metadata
+
+project = "ffsim"
+copyright = "2023, IBM"
+author = "Kevin J. Sung"
+release = importlib.metadata.version("ffsim")
+
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.extlinks",
+    "jupyter_sphinx",
+    "sphinx_autodoc_typehints",
+    "nbsphinx",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "**.ipynb_checkpoints"]
+
+
+# HTML output options
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+# autodoc/autosummary options
+autosummary_generate = True
+autosummary_generate_overwrite = False
+autoclass_content = "both"
+
+# nbsphinx options (for tutorials)
+nbsphinx_timeout = 500
+nbsphinx_execute = "always"
+nbsphinx_widgets_path = ""
