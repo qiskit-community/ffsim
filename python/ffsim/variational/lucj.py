c05b05f2dac79e39640436d6a7438b17e9335e7c
Kevin J. Sung <kevjsung@umich.edu>
2023-08-11 15:54:11 -0400

    document lucj and edit t amplitudes output

--- python/ffsim/variational/lucj.py
+++ python/ffsim/variational/lucj.py
@@ -46,6 +46,40 @@ def _decompose(t2, no, nv, nb, o_pairs, verbose=False):
 
 @dataclass
 class UnitaryClusterJastrowOp:
+    r"""A unitary cluster Jastrow operator.
+
+    A unitary cluster Jastrow (UCJ) operator has the form
+
+    .. math::
+
+        \prod_{k = 1}^L \mathcal{W_k} e^{i \mathcal{J}_k} \mathcal{W_k^\dagger}
+
+    where each :math:`\mathcal{W_k}` is an orbital rotation and each :math:`\mathcal{J}`
+    is a diagonal Coulomb operator of the form
+
+    .. math::
+
+        \mathcal{J} = \frac12\sum_{ij,\sigma \tau}
+        \mathbf{J}^{\sigma \tau}_{ij} n_{i,\sigma} n_{j,\tau}.
+
+    In order that the operator commutes with the total spin operator, we enforce that
+    :math:`\mathbf{J}^{\alpha\alpha} = \mathbf{J}^{\beta\beta}` and
+    :math:`\mathbf{J}^{\alpha\beta} = \mathbf{J}^{\beta\alpha}`. As a result, we have
+    two sets of matrices for describing the diagonal Coulomb operators:
+    "alpha-alpha" matrices containing coefficients for terms involving the same spin,
+    and "alpha-beta" matrices containing coefficients for terms involving different
+    spins.
+
+    To support variational optimization of the orbital basis, an optional final
+    orbital rotation can be included in the operator, to be performed at the end.
+
+    Attributes:
+        diag_coulomb_mats_alpha_alpha: The "alpha-alpha" diagonal Coulomb matrices.
+        diag_coulomb_mats_alpha_beta: The "alpha-beta" diagonal Coulomb matrices.
+        orbital_rotations: The orbital rotations.
+        final_orbital_rotation: The optional final orbital rotation.
+    """
+
     diag_coulomb_mats_alpha_alpha: np.ndarray
     diag_coulomb_mats_alpha_beta: np.ndarray
     orbital_rotations: np.ndarray
@@ -71,6 +105,7 @@ class UnitaryClusterJastrowOp:
         alpha_beta_indices: list[tuple[int, int]] | None = None,
         with_final_orbital_rotation: bool = False,
     ) -> "UnitaryClusterJastrowOp":
+        """Initialize the UCJ operator from a real-valued parameter vector."""
         triu_indices = list(itertools.combinations_with_replacement(range(norb), 2))
         triu_indices_no_diag = list(itertools.combinations(range(norb), 2))
         if alpha_alpha_indices is None:
@@ -157,6 +192,7 @@ class UnitaryClusterJastrowOp:
         alpha_alpha_indices: list[int] | None = None,
         alpha_beta_indices: list[int] | None = None,
     ) -> np.ndarray:
+        """Convert the UCJ operator to a real-valued parameter vector."""
         triu_indices = list(
             itertools.combinations_with_replacement(range(self.norb), 2)
         )
@@ -222,6 +258,7 @@ class UnitaryClusterJastrowOp:
         t1: np.ndarray | None = None,
         n_reps: int | None = None,
     ) -> "UnitaryClusterJastrowOp":
+        """Initialize the UCJ operator from t2 (and optionally t1) amplitudes."""
         # TODO allow specifying alpha-alpha and alpha-beta indices
         nocc, _, nvrt, _ = t2.shape
         norb = nocc + nvrt
@@ -261,7 +298,10 @@ class UnitaryClusterJastrowOp:
             final_orbital_rotation=final_orbital_rotation,
         )
 
-    def to_t_amplitudes(self, nocc: int | None = None) -> tuple[np.ndarray, np.ndarray]:
+    def to_t_amplitudes(
+        self, nocc: int | None = None
+    ) -> np.ndarray | tuple[np.ndarray, np.ndarray]:
+        """Convert the UCJ operator to t2 (and possibly t1) amplitudes."""
         t2 = np.zeros((self.norb, self.norb, self.norb, self.norb), dtype=complex)
         for mu in range(self.n_reps):
             Jmu = self.diag_coulomb_mats_alpha_beta[mu]
@@ -269,10 +309,13 @@ class UnitaryClusterJastrowOp:
             t2 += 1j * np.einsum(
                 "pq,ap,ip,bq,jq->aibj", Jmu, Vmu, np.conj(Vmu), Vmu, np.conj(Vmu)
             )
-        t1 = np.zeros((self.norb, self.norb))[:nocc, nocc:]
-        if self.final_orbital_rotation is not None:
-            t1 = scipy.linalg.logm(self.final_orbital_rotation)[:nocc, nocc:]
-        return np.einsum("aibj->ijab", t2)[:nocc, :nocc, nocc:, nocc:], t1
+        t2 = np.einsum("aibj->ijab", t2)[:nocc, :nocc, nocc:, nocc:]
+
+        if self.final_orbital_rotation is None:
+            return t2
+
+        t1 = scipy.linalg.logm(self.final_orbital_rotation)[:nocc, nocc:]
+        return t2, t1
 
 
 def apply_unitary_cluster_jastrow_op(
@@ -282,7 +325,22 @@ def apply_unitary_cluster_jastrow_op(
     norb: int,
     nelec: tuple[int, int],
     copy: bool = True,
-):
+) -> np.ndarray:
+    """Apply a UCJ operator to a vector.
+
+    Args:
+        vec: The state vector to be transformed.
+        operator: The UCJ operator.
+        norb: The number of spatial orbitals.
+        nelec: The number of alpha and beta electrons.
+        copy: Whether to copy the vector before operating on it.
+            - If ``copy=True`` then this function always returns a newly allocated
+            vector and the original vector is left untouched.
+            - If ``copy=False`` then this function may still return a newly allocated
+            vector, but the original vector may have its data overwritten.
+            It is also possible that the original vector is returned,
+            modified in-place.
+    """
     if copy:
         vec = vec.copy()
     for mat, mat_alpha_beta, orbital_rotation in zip(
