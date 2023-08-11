b85bec0ddc9da26ab680bdca6ed59cd362d46b48
Kevin J. Sung <kevjsung@umich.edu>
2023-08-11 17:22:48 -0400

    mypy

--- python/ffsim/states.py
+++ python/ffsim/states.py
@@ -17,7 +17,7 @@ from pyscf.fci import cistring
 from scipy.special import comb
 
 
-def dims(norb: int, nelec: tuple[int, int]) -> int:
+def dims(norb: int, nelec: tuple[int, int]) -> tuple[int, int]:
     """Get the dimension of the FCI space."""
     n_alpha, n_beta = nelec
     dim_a = comb(norb, n_alpha, exact=True)
