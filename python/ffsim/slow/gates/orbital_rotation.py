abb2b073e5ed02735f21fa5e7be58d9c7ebb7398
Kevin J. Sung <kevjsung@umich.edu>
2023-09-19 19:53:22 -0400

    split slow into modules (#12)

---
+++ python/ffsim/slow/gates/orbital_rotation.py
@@ -0,0 +1,80 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from __future__ import annotations
+
+import numpy as np
+
+
+def gen_orbital_rotation_index_in_place_slow(
+    norb: int,
+    nocc: int,
+    linkstr_index: np.ndarray,
+    diag_strings: np.ndarray,
+    off_diag_strings: np.ndarray,
+    off_diag_strings_index: np.ndarray,
+    off_diag_index: np.ndarray,
+) -> None:
+    """Generate orbital rotation index."""
+    diag_counter = np.zeros(norb, dtype=np.uint)
+    off_diag_counter = np.zeros(norb, dtype=np.uint)
+    for str0, tab in enumerate(linkstr_index[:, :, 0]):
+        for orb in tab[:nocc]:
+            count = diag_counter[orb]
+            diag_strings[orb, count] = str0
+            diag_counter[orb] += 1
+        for orb in tab[nocc:norb]:
+            count = off_diag_counter[orb]
+            off_diag_strings[orb, count] = str0
+            off_diag_strings_index[orb, str0] = count
+            off_diag_counter[orb] += 1
+
+    index_counter = np.zeros_like(off_diag_strings)
+    for str0, tab in enumerate(linkstr_index):
+        for orb_c, orb_d, str1, sign in tab[nocc:]:
+            # str0 -> annihilate orb_d -> create orb_c -> str1
+            index = off_diag_strings_index[orb_c, str0]
+            count = index_counter[orb_c, index]
+            off_diag_index[orb_c, index, count] = orb_d, str1, sign
+            index_counter[orb_c, index] += 1
+
+
+def apply_single_column_transformation_in_place_slow(
+    vec: np.ndarray,
+    column: np.ndarray,
+    diag_val: complex,
+    diag_strings: np.ndarray,
+    off_diag_strings: np.ndarray,
+    off_diag_index: np.ndarray,
+) -> None:
+    """Apply a single-column orbital rotation."""
+    for str0, tab in zip(off_diag_strings, off_diag_index):
+        for orb, str1, sign in tab:
+            vec[str0] += sign * column[orb] * vec[str1]
+    for str0 in diag_strings:
+        vec[str0] *= diag_val
+
+
+def apply_givens_rotation_in_place_slow(
+    vec: np.ndarray,
+    c: float,
+    s: float,
+    phase: complex,
+    slice1: np.ndarray,
+    slice2: np.ndarray,
+) -> None:
+    """Apply a Givens rotation to slices of a state vector."""
+    phase_conj = phase.conjugate()
+    for i, j in zip(slice1, slice2):
+        vec[i] *= phase_conj
+        tmp = c * vec[i] + s * vec[j]
+        vec[j] = c * vec[j] - s * vec[i]
+        vec[i] = tmp
+        vec[i] *= phase
---
+++ tests/slow/contract/num_op_sum_test.py
@@ -0,0 +1,48 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Tests for gates."""
+
+from __future__ import annotations
+
+import numpy as np
+from pyscf.fci import cistring
+from scipy.special import comb
+
+import ffsim
+from ffsim._ffsim import contract_num_op_sum_spin_into_buffer
+from ffsim.slow.contract.num_op_sum import contract_num_op_sum_spin_into_buffer_slow
+
+
+def test_contract_num_op_sum_spin_into_buffer_slow():
+    """Test applying num op sum evolution."""
+    norb = 5
+    rng = np.random.default_rng()
+    for _ in range(5):
+        n_alpha = rng.integers(1, norb + 1)
+        n_beta = rng.integers(1, norb + 1)
+        dim_a = comb(norb, n_alpha, exact=True)
+        dim_b = comb(norb, n_beta, exact=True)
+        occupations = cistring.gen_occslst(range(norb), n_alpha).astype(
+            np.uint, copy=False
+        )
+        coeffs = np.random.uniform(size=norb)
+        vec = ffsim.random.random_statevector(dim_a * dim_b, seed=rng).reshape(
+            (dim_a, dim_b)
+        )
+        out_slow = np.zeros_like(vec)
+        out_fast = np.zeros_like(vec)
+        contract_num_op_sum_spin_into_buffer_slow(
+            vec, coeffs, occupations=occupations, out=out_slow
+        )
+        contract_num_op_sum_spin_into_buffer(
+            vec, coeffs, occupations=occupations, out=out_fast
+        )
+        np.testing.assert_allclose(out_slow, out_fast, atol=1e-8)
