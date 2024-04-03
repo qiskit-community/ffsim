c0b1fd76dd5920371d7fcf1d9af14230790bb5e5
Kevin J. Sung <kevjsung@umich.edu>
2024-04-03 09:03:02 +0800

    python 3.8 doesn't have @=

--- tests/python/linalg/givens_test.py
+++ tests/python/linalg/givens_test.py
@@ -36,7 +36,8 @@ def test_givens_decomposition_definition(dim: int):
                 [c, s],
                 [-s.conjugate(), c],
             ]
-            reconstructed @= givens_mat.conj()
+            # TODO use in-place operator @= after Python 3.9
+            reconstructed = reconstructed @ givens_mat.conj()
         np.testing.assert_allclose(reconstructed, mat)
         assert len(givens_rotations) == dim * (dim - 1) // 2
 
