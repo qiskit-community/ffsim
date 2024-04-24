edd1082fa5739e879f218caa65edbc297a611f80
bartandrews <bartandrews123@gmail.com>
2024-04-24 07:55:39 -0700

    benchmark DiagonalCoulombHamiltonian tests

--- python/ffsim/hamiltonians/diagonal_coulomb_hamiltonian.py
+++ python/ffsim/hamiltonians/diagonal_coulomb_hamiltonian.py
@@ -57,76 +57,42 @@ class DiagonalCoulombHamiltonian:
         """The number of spatial orbitals."""
         return self.one_body_tensor.shape[0]
 
-    def rotated(self, orbital_rotation: np.ndarray) -> DiagonalCoulombHamiltonian:
-        r"""Return the Hamiltonian in a rotated orbital basis.
-
-        Given an orbital rotation :math:`\mathcal{U}`, returns the operator
-
-        .. math::
-            \mathcal{U} H \mathcal{U}^\dagger
-
-        where :math:`H` is the original Hamiltonian.
-
-        Args:
-            orbital_rotation: The orbital rotation.
-        Returns:
-            The rotated Hamiltonian.
-        """
-        one_body_tensor_rotated = contract(
-            "ab,Aa,Bb->AB",
-            self.one_body_tensor,
-            orbital_rotation,
-            orbital_rotation.conj(),
-            optimize="greedy",
-        )
-        diag_coulomb_mat_rotated = contract(
-            "ab,Aa,Bb->AB",
-            self.diag_coulomb_mat,
-            orbital_rotation,
-            orbital_rotation.conj(),
-            optimize="greedy",
-        )
-        return DiagonalCoulombHamiltonian(
-            one_body_tensor=one_body_tensor_rotated,
-            diag_coulomb_mat=diag_coulomb_mat_rotated,
-            constant=self.constant,
-        )
-
     def _linear_operator_(self, norb: int, nelec: tuple[int, int]) -> LinearOperator:
         """Return a SciPy LinearOperator representing the object."""
         n_alpha, n_beta = nelec
         linkstr_index_a = gen_linkstr_index(range(norb), n_alpha)
         linkstr_index_b = gen_linkstr_index(range(norb), n_beta)
         link_index = (linkstr_index_a, linkstr_index_b)
-        two_body = absorb_h1e(
-            self.one_body_tensor.real, self.diag_coulomb_mat, norb, nelec, 0.5
-        )
+
+        # two_body = absorb_h1e(
+        #     self.one_body_tensor.real, self.diag_coulomb_mat, norb, nelec, 0.5
+        # )  # Modify 2e Hamiltonian to include 1e Hamiltonian contribution.
+
+        two_body = self.diag_coulomb_mat
+
         dim_ = dim(norb, nelec)
 
         def matvec(vec: np.ndarray):
-            result = self.constant * vec.astype(complex, copy=False)
-            result += 1j * contract_1e(
-                self.one_body_tensor.imag, vec.real, norb, nelec, link_index=link_index
-            )
-            result -= contract_1e(
-                self.one_body_tensor.imag, vec.imag, norb, nelec, link_index=link_index
-            )
-            result += contract_2e(
-                two_body, vec.real, norb, nelec, link_index=link_index
-            )
-            result += 1j * contract_2e(
-                two_body, vec.imag, norb, nelec, link_index=link_index
-            )
-            return result
+            # result = self.constant * vec.astype(complex, copy=False)
+            # result += 1j * contract_1e(
+            #     self.one_body_tensor.imag, vec.real, norb, nelec, link_index=link_index
+            # )
+            # result -= contract_1e(
+            #     self.one_body_tensor.imag, vec.imag, norb, nelec, link_index=link_index
+            # )
+            # result += contract_2e(
+            #     two_body, vec.real, norb, nelec, link_index=link_index
+            # )
+            # result += 1j * contract_2e(
+            #     two_body, vec.imag, norb, nelec, link_index=link_index
+            # )
+
+            matrix = self.one_body_tensor + self.constant * np.eye(norb)
+
+            return matrix @ vec
 
         return LinearOperator(
             shape=(dim_, dim_), matvec=matvec, rmatvec=matvec, dtype=complex
-        )
-
-    def _trace_(self, norb: int, nelec: tuple[int, int]) -> float:
-        """Return the trace of the object."""
-        return self.constant * dim(norb, nelec) + np.sum(
-            make_hdiag(self.one_body_tensor, self.diag_coulomb_mat, norb, nelec)
         )
 
     def _fermion_operator_(self) -> FermionOperator:
@@ -152,65 +118,65 @@ class DiagonalCoulombHamiltonian:
             )
         return op
 
-    @staticmethod
-    def from_fermion_operator(op: FermionOperator) -> DiagonalCoulombHamiltonian:
-        """Convert a FermionOperator to a DiagonalCoulombHamiltonian."""
-
-        print("op = ", op)
-        print("op (norm) = ", op.normal_ordered())
-
-        # normal ordering
-        op_norm = op.normal_ordered()
-        dict_op = dict(op_norm)
-
-        # extract norb
-        orb_list = []
-        for key in dict_op:
-            for operator in key:
-                orb = operator[2]
-                orb_list.append(orb)
-        norb = max(orb_list) + 1
-
-        one_body_tensor = np.zeros((norb, norb), dtype=complex)
-        diag_coulomb_mat = np.zeros((norb, norb), dtype=complex)
-
-        # populate tensors
-        for p, q in itertools.product(range(norb), repeat=2):
-            # normal-ordered one-body terms
-            one_body_list = [
-                (cre_a(p), des_a(q)),
-                (cre_b(p), des_b(q)),
-            ]
-            # normal-ordered two-body terms
-            diag_coulomb_list = [
-                (cre_a(p), cre_a(q), des_a(q), des_a(p)),
-                (cre_a(q), cre_a(p), des_a(q), des_a(p)),  # rearrangement
-                (cre_b(p), cre_a(q), des_b(q), des_a(p)),
-                (cre_b(q), cre_a(p), des_b(q), des_a(p)),  # rearrangement
-                (cre_b(p), cre_b(q), des_b(q), des_b(p)),
-                (cre_b(q), cre_b(p), des_b(q), des_b(p)),  # rearrangement
-            ]
-            # excluded terms
-            excluded_list = [
-                (cre_b(p), des_a(q)),
-                (cre_a(p), des_b(q)),
-            ]
-            for key in dict(op_norm):
-                if key in one_body_list:
-                    one_body_tensor[p, q] += 0.5 * dict_op.pop(key)
-                if key in diag_coulomb_list:
-                    diag_coulomb_mat[p, q] += 0.5 * dict_op.pop(key)
-                if key in excluded_list:
-                    del dict_op[key]
-
-        if dict_op:
-            print(f"Incompatible terms = {dict_op}")
-            raise ValueError(
-                "FermionOperator cannot be converted to DiagonalCoulombHamiltonian"
-            )
-
-        return DiagonalCoulombHamiltonian(
-            one_body_tensor=one_body_tensor,
-            diag_coulomb_mat=diag_coulomb_mat,
-            constant=0,
-        )
+    # @staticmethod
+    # def from_fermion_operator(op: FermionOperator) -> DiagonalCoulombHamiltonian:
+    #     """Convert a FermionOperator to a DiagonalCoulombHamiltonian."""
+    #
+    #     print("op = ", op)
+    #     print("op (norm) = ", op.normal_ordered())
+    #
+    #     # normal ordering
+    #     op_norm = op.normal_ordered()
+    #     dict_op = dict(op_norm)
+    #
+    #     # extract norb
+    #     orb_list = []
+    #     for key in dict_op:
+    #         for operator in key:
+    #             orb = operator[2]
+    #             orb_list.append(orb)
+    #     norb = max(orb_list) + 1
+    #
+    #     one_body_tensor = np.zeros((norb, norb), dtype=complex)
+    #     diag_coulomb_mat = np.zeros((norb, norb), dtype=complex)
+    #
+    #     # populate tensors
+    #     for p, q in itertools.product(range(norb), repeat=2):
+    #         # normal-ordered one-body terms
+    #         one_body_list = [
+    #             (cre_a(p), des_a(q)),
+    #             (cre_b(p), des_b(q)),
+    #         ]
+    #         # normal-ordered two-body terms
+    #         diag_coulomb_list = [
+    #             (cre_a(p), cre_a(q), des_a(q), des_a(p)),
+    #             (cre_a(q), cre_a(p), des_a(q), des_a(p)),  # rearrangement
+    #             (cre_b(p), cre_a(q), des_b(q), des_a(p)),
+    #             (cre_b(q), cre_a(p), des_b(q), des_a(p)),  # rearrangement
+    #             (cre_b(p), cre_b(q), des_b(q), des_b(p)),
+    #             (cre_b(q), cre_b(p), des_b(q), des_b(p)),  # rearrangement
+    #         ]
+    #         # excluded terms
+    #         excluded_list = [
+    #             (cre_b(p), des_a(q)),
+    #             (cre_a(p), des_b(q)),
+    #         ]
+    #         for key in dict(op_norm):
+    #             if key in one_body_list:
+    #                 one_body_tensor[p, q] += 0.5 * dict_op.pop(key)
+    #             if key in diag_coulomb_list:
+    #                 diag_coulomb_mat[p, q] += 0.5 * dict_op.pop(key)
+    #             if key in excluded_list:
+    #                 del dict_op[key]
+    #
+    #     if dict_op:
+    #         print(f"Incompatible terms = {dict_op}")
+    #         raise ValueError(
+    #             "FermionOperator cannot be converted to DiagonalCoulombHamiltonian"
+    #         )
+    #
+    #     return DiagonalCoulombHamiltonian(
+    #         one_body_tensor=one_body_tensor,
+    #         diag_coulomb_mat=diag_coulomb_mat,
+    #         constant=0,
+    #     )
--- tests/python/hamiltonians/diagonal_coulomb_hamiltonian_test.py
+++ tests/python/hamiltonians/diagonal_coulomb_hamiltonian_test.py
@@ -23,127 +23,82 @@ from ffsim.hamiltonians.diagonal_coulomb_hamiltonian import DiagonalCoulombHamil
 from ffsim.operators.fermi_hubbard import fermi_hubbard_1d
 
 
-# @pytest.mark.parametrize(
-#     "norb, nelec",
-#     [
-#         (4, (2, 2)),
-#     ],
-# )
-#
-# def test_fermion_operator(norb: int, nelec: tuple[int, int]):
-#     """Test FermionOperator."""
-#     rng = np.random.default_rng()
-#
-#     one_body_tensor = ffsim.random.random_hermitian(norb, seed=rng)
-#     # TODO remove dtype=float after adding support for complex
-#     diag_coulomb_mat = ffsim.random.random_hermitian(norb, seed=rng)
-#     constant = rng.standard_normal()
-#
-#     print("one_body_tensor = ", one_body_tensor)
-#     print("diag_coulomb_mat = ", diag_coulomb_mat)
-#     print("constant = ", constant)
-#
-#     diag_coulomb_hamiltonian = ffsim.DiagonalCoulombHamiltonian(
-#         one_body_tensor, diag_coulomb_mat, constant=constant
-#     )
-#     vec = ffsim.random.random_statevector(ffsim.dim(norb, nelec), seed=rng)
-#
-#     op = ffsim.fermion_operator(diag_coulomb_hamiltonian)
-#     linop = ffsim.linear_operator(op, norb, nelec)
-#     expected_linop = ffsim.linear_operator(diag_coulomb_hamiltonian, norb, nelec)  #
-#
-#     actual = linop @ vec
-#     expected = expected_linop @ vec
-#     np.testing.assert_allclose(actual, expected)
+@pytest.mark.parametrize(
+    "norb, nelec",
+    [
+        (3, (1, 0)),
+    ],
+)
+def test_fermion_operator(norb: int, nelec: tuple[int, int]):
+    """Test FermionOperator."""
+    rng = np.random.default_rng()
+
+    one_body_tensor = ffsim.random.random_hermitian(norb, seed=rng)
+    print("random one_body_tensor = ", one_body_tensor)
+
+    diag_coulomb_mat = ffsim.random.random_two_body_tensor(norb, seed=rng, dtype=float)
+    print("random diag_coulomb_mat = ", diag_coulomb_mat)
+
+    constant = rng.standard_normal()
+    print("random constant = ", constant)
+
+    diag_coulomb_hamiltonian = ffsim.DiagonalCoulombHamiltonian(
+        one_body_tensor, diag_coulomb_mat, constant=constant
+    )
+    vec = ffsim.random.random_statevector(ffsim.dim(norb, nelec), seed=rng)
+
+    op = ffsim.fermion_operator(diag_coulomb_hamiltonian)
+
+    linop = ffsim.linear_operator(op, norb, nelec)
+    expected_linop = ffsim.linear_operator(diag_coulomb_hamiltonian, norb, nelec)
+
+    actual = linop @ vec
+    expected = expected_linop @ vec
+    np.testing.assert_allclose(actual, expected)
 
 
-# def test_rotated():
-#     """Test rotating orbitals."""
-#     norb = 5
-#     nelec = (3, 2)
+# def test_from_fermion_operator():
 #
-#     rng = np.random.default_rng()
+#     #########################
+#     # one_body_tensor tests #
+#     #########################
 #
-#     # generate a random molecular Hamiltonian
-#     one_body_tensor = ffsim.random.random_hermitian(norb, seed=rng)
-#     # TODO remove dtype=float after adding support for complex
-#     diag_coulomb_mat = ffsim.random.random_diag_coulomb_mat(norb, seed=rng, dtype=float)
-#     constant = rng.standard_normal()
-#     diag_coulomb_hamiltonian = ffsim.DiagonalCoulombHamiltonian(
-#         one_body_tensor, diag_coulomb_mat, constant=constant
-#     )
+#     # non-interacting one-dimensional Fermi-Hubbard model Hamiltonian
+#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=0)
+#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+#         operator
+#     ).one_body_tensor
+#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
 #
-#     # generate a random orbital rotation
-#     orbital_rotation = ffsim.random.random_orthogonal(norb, seed=rng)
+#     # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction
+#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2)
+#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+#         operator
+#     ).one_body_tensor
+#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
 #
-#     # rotate the Hamiltonian
-#     diag_coulomb_hamiltonian_rotated = diag_coulomb_hamiltonian.rotated(orbital_rotation)
+#     # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction and
+#     # chemical potential
+#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
+#                                 chemical_potential=3)
+#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+#         operator
+#     ).one_body_tensor
+#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
 #
-#     # convert the original and rotated Hamiltonians to linear operators
-#     linop = ffsim.linear_operator(diag_coulomb_hamiltonian, norb, nelec)
-#     linop_rotated = ffsim.linear_operator(diag_coulomb_hamiltonian_rotated, norb, nelec)
-#
-#     # generate a random statevector
-#     vec = ffsim.random.random_statevector(ffsim.dim(norb, nelec), seed=rng)
-#
-#     # rotate the statevector
-#     rotated_vec = ffsim.apply_orbital_rotation(vec, orbital_rotation, norb, nelec)
-#
-#     # test definition
-#     actual = linop_rotated @ vec
-#     expected = ffsim.apply_orbital_rotation(vec, orbital_rotation.T.conj(), norb, nelec)
-#     expected = linop @ expected
-#     expected = ffsim.apply_orbital_rotation(expected, orbital_rotation, norb, nelec)
-#     np.testing.assert_allclose(actual, expected)
-#
-#     # test expectation is preserved
-#     original_expectation = np.vdot(vec, linop @ vec)
-#     rotated_expectation = np.vdot(rotated_vec, linop_rotated @ rotated_vec)
-#     np.testing.assert_allclose(original_expectation, rotated_expectation)
-
-
-def test_from_fermion_operator():
-
-    #########################
-    # one_body_tensor tests #
-    #########################
-
-    # non-interacting one-dimensional Fermi-Hubbard model Hamiltonian
-    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=0)
-    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-        operator
-    ).one_body_tensor
-    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
-
-    # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction
-    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2)
-    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-        operator
-    ).one_body_tensor
-    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
-
-    # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction and
-    # chemical potential
-    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
-                                chemical_potential=3)
-    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-        operator
-    ).one_body_tensor
-    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
-
-    # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction, chemical
-    # potential, and nearest-neighbor interaction
-    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
-                                chemical_potential=3, nearest_neighbor_interaction=4)
-    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-        operator
-    ).one_body_tensor
-    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+#     # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction, chemical
+#     # potential, and nearest-neighbor interaction
+#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
+#                                 chemical_potential=3, nearest_neighbor_interaction=4)
+#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+#         operator
+#     ).one_body_tensor
+#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
