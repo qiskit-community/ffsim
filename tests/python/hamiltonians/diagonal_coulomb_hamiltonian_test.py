74681828a9d7c90ae1eef59c2e4f7683bb5e5045
bartandrews <bartandrews123@gmail.com>
2024-04-25 22:02:36 -0700

    restore one_body_tensor tests

--- tests/python/hamiltonians/diagonal_coulomb_hamiltonian_test.py
+++ tests/python/hamiltonians/diagonal_coulomb_hamiltonian_test.py
@@ -23,82 +23,82 @@ from ffsim.hamiltonians.diagonal_coulomb_hamiltonian import DiagonalCoulombHamil
 from ffsim.operators.fermi_hubbard import fermi_hubbard_1d
 
 
-@pytest.mark.parametrize(
-    "norb, nelec",
-    [
-        (3, (1, 0)),
-    ],
-)
-def test_fermion_operator(norb: int, nelec: tuple[int, int]):
-    """Test FermionOperator."""
-    rng = np.random.default_rng()
-
-    one_body_tensor = ffsim.random.random_hermitian(norb, seed=rng)
-    print("random one_body_tensor = ", one_body_tensor)
-
-    diag_coulomb_mat = ffsim.random.random_two_body_tensor(norb, seed=rng, dtype=float)
-    print("random diag_coulomb_mat = ", diag_coulomb_mat)
-
-    constant = rng.standard_normal()
-    print("random constant = ", constant)
-
-    diag_coulomb_hamiltonian = ffsim.DiagonalCoulombHamiltonian(
-        one_body_tensor, diag_coulomb_mat, constant=constant
-    )
-    vec = ffsim.random.random_statevector(ffsim.dim(norb, nelec), seed=rng)
-
-    op = ffsim.fermion_operator(diag_coulomb_hamiltonian)
-
-    linop = ffsim.linear_operator(op, norb, nelec)
-    expected_linop = ffsim.linear_operator(diag_coulomb_hamiltonian, norb, nelec)
-
-    actual = linop @ vec
-    expected = expected_linop @ vec
-    np.testing.assert_allclose(actual, expected)
-
-
-# def test_from_fermion_operator():
+# @pytest.mark.parametrize(
+#     "norb, nelec",
+#     [
+#         (3, (1, 0)),
+#     ],
+# )
+# def test_fermion_operator(norb: int, nelec: tuple[int, int]):
+#     """Test FermionOperator."""
+#     rng = np.random.default_rng()
 #
-#     #########################
-#     # one_body_tensor tests #
-#     #########################
+#     one_body_tensor = ffsim.random.random_hermitian(norb, seed=rng)
+#     print("random one_body_tensor = ", one_body_tensor)
 #
-#     # non-interacting one-dimensional Fermi-Hubbard model Hamiltonian
-#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=0)
-#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-#         operator
-#     ).one_body_tensor
-#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+#     diag_coulomb_mat = ffsim.random.random_two_body_tensor(norb, seed=rng, dtype=float)
+#     print("random diag_coulomb_mat = ", diag_coulomb_mat)
 #
-#     # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction
-#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2)
-#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-#         operator
-#     ).one_body_tensor
-#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+#     constant = rng.standard_normal()
+#     print("random constant = ", constant)
 #
-#     # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction and
-#     # chemical potential
-#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
-#                                 chemical_potential=3)
-#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-#         operator
-#     ).one_body_tensor
-#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+#     diag_coulomb_hamiltonian = ffsim.DiagonalCoulombHamiltonian(
+#         one_body_tensor, diag_coulomb_mat, constant=constant
+#     )
+#     vec = ffsim.random.random_statevector(ffsim.dim(norb, nelec), seed=rng)
 #
-#     # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction, chemical
-#     # potential, and nearest-neighbor interaction
-#     operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
-#                                 chemical_potential=3, nearest_neighbor_interaction=4)
-#     hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
-#     one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
-#     one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
-#         operator
-#     ).one_body_tensor
-#     np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+#     op = ffsim.fermion_operator(diag_coulomb_hamiltonian)
+#
+#     linop = ffsim.linear_operator(op, norb, nelec)
+#     expected_linop = ffsim.linear_operator(diag_coulomb_hamiltonian, norb, nelec)
+#
+#     actual = linop @ vec
+#     expected = expected_linop @ vec
+#     np.testing.assert_allclose(actual, expected)
+
+
+def test_from_fermion_operator():
+
+    #########################
+    # one_body_tensor tests #
+    #########################
+
+    # non-interacting one-dimensional Fermi-Hubbard model Hamiltonian
+    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=0)
+    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+        operator
+    ).one_body_tensor
+    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+
+    # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction
+    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2)
+    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+        operator
+    ).one_body_tensor
+    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+
+    # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction and
+    # chemical potential
+    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
+                                chemical_potential=3)
+    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+        operator
+    ).one_body_tensor
+    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
+
+    # one-dimensional Fermi-Hubbard model Hamiltonian with onsite interaction, chemical
+    # potential, and nearest-neighbor interaction
+    operator = fermi_hubbard_1d(norb=4, tunneling=1, interaction=2,
+                                chemical_potential=3, nearest_neighbor_interaction=4)
+    hamiltonian = ffsim.linear_operator(operator, norb=4, nelec=(1, 0))
+    one_body_tensor_1 = hamiltonian.matmat(np.eye(4))
+    one_body_tensor_2 = DiagonalCoulombHamiltonian.from_fermion_operator(
+        operator
+    ).one_body_tensor
+    np.testing.assert_allclose(one_body_tensor_1, one_body_tensor_2)
