b442467b21e586afcb5fdf7e387b71116ca31980
Kevin J. Sung <kevjsung@umich.edu>
2024-05-01 12:03:02 -0400

    organize qiskit module into submodules (#159)

--- python/ffsim/qiskit/gates/__init__.py
+++ python/ffsim/qiskit/gates/__init__.py
@@ -8,26 +8,20 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Code that uses Qiskit, e.g. for constructing quantum circuits."""
+"""Qiskit fermionic quantum gates."""
 
-from ffsim.qiskit.gates import (
-    DiagCoulombEvolutionJW,
-    OrbitalRotationJW,
+from ffsim.qiskit.gates.diag_coulomb import DiagCoulombEvolutionJW
+from ffsim.qiskit.gates.orbital_rotation import OrbitalRotationJW
+from ffsim.qiskit.gates.slater_determinant import (
     PrepareHartreeFockJW,
     PrepareSlaterDeterminantJW,
-    UCJOperatorJW,
 )
-from ffsim.qiskit.transpiler_passes import DropNegligible, MergeOrbitalRotations
-from ffsim.qiskit.util import ffsim_vec_to_qiskit_vec, qiskit_vec_to_ffsim_vec
+from ffsim.qiskit.gates.ucj import UCJOperatorJW
 
 __all__ = [
     "DiagCoulombEvolutionJW",
-    "DropNegligible",
-    "MergeOrbitalRotations",
     "OrbitalRotationJW",
     "PrepareHartreeFockJW",
     "PrepareSlaterDeterminantJW",
     "UCJOperatorJW",
-    "ffsim_vec_to_qiskit_vec",
-    "qiskit_vec_to_ffsim_vec",
 ]
