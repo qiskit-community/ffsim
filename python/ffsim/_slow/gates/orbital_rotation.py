e149860b48624c1a4677e04b2d49f0f6943978c8
Kevin J. Sung <kevjsung@umich.edu>
2024-05-25 12:41:57 -0400

    remove code related to LU decomposition orbital rotation (#191)

--- python/ffsim/_slow/gates/orbital_rotation.py
+++ python/ffsim/_slow/gates/orbital_rotation.py
@@ -14,55 +14,6 @@ import numpy as np
 from scipy.linalg.lapack import zrot
 
 
-def gen_orbital_rotation_index_in_place_slow(
-    norb: int,
-    nocc: int,
-    linkstr_index: np.ndarray,
-    diag_strings: np.ndarray,
-    off_diag_strings: np.ndarray,
-    off_diag_strings_index: np.ndarray,
-    off_diag_index: np.ndarray,
-) -> None:
-    """Generate orbital rotation index."""
-    diag_counter = np.zeros(norb, dtype=np.uint)
-    off_diag_counter = np.zeros(norb, dtype=np.uint)
-    for str0, tab in enumerate(linkstr_index[:, :, 0]):
-        for orb in tab[:nocc]:
-            count = diag_counter[orb]
-            diag_strings[orb, count] = str0
-            diag_counter[orb] += 1
-        for orb in tab[nocc:norb]:
-            count = off_diag_counter[orb]
-            off_diag_strings[orb, count] = str0
-            off_diag_strings_index[orb, str0] = count
-            off_diag_counter[orb] += 1
-
-    index_counter = np.zeros_like(off_diag_strings)
-    for str0, tab in enumerate(linkstr_index):
-        for orb_c, orb_d, str1, sign in tab[nocc:]:
-            # str0 -> annihilate orb_d -> create orb_c -> str1
-            index = off_diag_strings_index[orb_c, str0]
-            count = index_counter[orb_c, index]
-            off_diag_index[orb_c, index, count] = orb_d, str1, sign
-            index_counter[orb_c, index] += 1
-
-
-def apply_single_column_transformation_in_place_slow(
-    vec: np.ndarray,
-    column: np.ndarray,
-    diag_val: complex,
-    diag_strings: np.ndarray,
-    off_diag_strings: np.ndarray,
-    off_diag_index: np.ndarray,
-) -> None:
-    """Apply a single-column orbital rotation."""
-    for str0, tab in zip(off_diag_strings, off_diag_index):
-        for orb, str1, sign in tab:
-            vec[str0] += sign * column[orb] * vec[str1]
-    for str0 in diag_strings:
-        vec[str0] *= diag_val
-
-
 def apply_givens_rotation_in_place_slow(
     vec: np.ndarray,
     c: float,
--- python/ffsim/cistring.py
+++ python/ffsim/cistring.py
@@ -12,13 +12,10 @@
 
 from __future__ import annotations
 
-import math
 from functools import lru_cache
 
 import numpy as np
 from pyscf.fci import cistring
-
-from ffsim._lib import gen_orbital_rotation_index_in_place
 
 
 @lru_cache(maxsize=None)
@@ -39,55 +36,6 @@ def gen_linkstr_index(orbitals: range, nocc: int):
     return cistring.gen_linkstr_index(orbitals, nocc)
 
 
-@lru_cache(maxsize=None)
-def gen_orbital_rotation_index(
-    norb: int, nocc: int
-) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Generate string index used for performing orbital rotations.
-
-    Returns a tuple (diag_strings, off_diag_strings, off_diag_index)
-    of three Numpy arrays.
-
-    diag_strings is a norb x binom(norb - 1, nocc - 1) array.
-    The i-th row of this array contains all the strings with orbital i occupied.
-
-    off_diag_strings is a norb x binom(norb - 1, nocc) array.
-    The i-th row of this array contains all the strings with orbital i unoccupied.
-
-    off_diag_index is a norb x binom(norb - 1, nocc) x nocc x 3 array.
-    The first two axes of this array are in one-to-one correspondence with
-    off_diag_strings. For a fixed choice (i, str0) for the first two axes,
-    the last two axes form a nocc x 3 array. Each row of this array is a tuple
-    (j, str1, sign) where str1 is formed by annihilating orbital j in str0 and creating
-    orbital i, with sign giving the fermionic parity of this operation.
-    """
-    if nocc == 0:
-        diag_strings = np.zeros((norb, 0), dtype=np.uint)
-        off_diag_strings = np.zeros((norb, 1), dtype=np.uint)
-        off_diag_index = np.zeros((norb, 1, 0, 3), dtype=np.int32)
-        return diag_strings, off_diag_strings, off_diag_index
-
-    linkstr_index = gen_linkstr_index(range(norb), nocc)
-    dim_diag = math.comb(norb - 1, nocc - 1)
-    dim_off_diag = math.comb(norb - 1, nocc)
-    dim = dim_diag + dim_off_diag
-    diag_strings = np.empty((norb, dim_diag), dtype=np.uint)
-    off_diag_strings = np.empty((norb, dim_off_diag), dtype=np.uint)
-    # TODO should this be int64? pyscf uses int32 for linkstr_index though
-    off_diag_index = np.empty((norb, dim_off_diag, nocc, 3), dtype=np.int32)
-    off_diag_strings_index = np.empty((norb, dim), dtype=np.uint)
-    gen_orbital_rotation_index_in_place(
-        norb=norb,
-        nocc=nocc,
-        linkstr_index=linkstr_index,
-        diag_strings=diag_strings,
-        off_diag_strings=off_diag_strings,
-        off_diag_strings_index=off_diag_strings_index,
-        off_diag_index=off_diag_index,
-    )
-    return diag_strings, off_diag_strings, off_diag_index
-
-
 def init_cache(norb: int, nelec: tuple[int, int]) -> None:
     """Initialize cached objects.
 
@@ -104,4 +52,3 @@ def init_cache(norb: int, nelec: tuple[int, int]) -> None:
         make_strings(range(norb), nocc)
         gen_occslst(range(norb), nocc)
         gen_linkstr_index(range(norb), nocc)
-        gen_orbital_rotation_index(norb, nocc)
