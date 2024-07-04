56b454ad6909f205f22406931bc3314b8d91a346
Kevin J. Sung <kevjsung@umich.edu>
2024-07-03 21:12:43 -0400

    split out trotter utils

--- python/ffsim/trotter/__init__.py
+++ python/ffsim/trotter/__init__.py
@@ -10,8 +10,8 @@
 
 """Hamiltonian simulation via Trotter-Suzuki formulas."""
 
+from ffsim.trotter.double_factorized import simulate_trotter_double_factorized
 from ffsim.trotter.qdrift import simulate_qdrift_double_factorized
-from ffsim.trotter.trotter import simulate_trotter_double_factorized
 
 __all__ = [
     "simulate_qdrift_double_factorized",
