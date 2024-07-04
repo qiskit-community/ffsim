ed884d76448fbda5bf6cb3e511a441bb57677167
Kevin J. Sung <kevjsung@umich.edu>
2024-07-03 21:57:58 -0400

    fix test doc

--- tests/python/trotter/double_factorized_test.py
+++ tests/python/trotter/double_factorized_test.py
@@ -8,7 +8,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Tests for Trotter simulation."""
+"""Tests for double-factorized Trotter simulation."""
 
 from __future__ import annotations
 
