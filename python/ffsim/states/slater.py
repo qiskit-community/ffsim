bcf6eb782d084d1ff7b5560b5736b68f0c40a72e
Kevin J. Sung <kevjsung@umich.edu>
2024-07-26 17:14:25 -0400

    fix docstrings and minor refactoring

--- python/ffsim/states/slater.py
+++ python/ffsim/states/slater.py
@@ -73,38 +73,32 @@ def sample_slater_determinant(
     rng = np.random.default_rng(seed)
 
     if isinstance(nelec, int):
-        # spinless case
+        # Spinless case.
         rdm = cast(np.ndarray, rdm)
         norb, _ = rdm.shape
-
         if orbs is None:
             orbs = range(norb)
         orbs = cast(Sequence[int], orbs)
-
-        strings = _sample_spinless_direct(rdm, nelec, shots, rng)
+        strings = _sample_slater_spinless(rdm, nelec, shots, rng)
         strings = restrict_bitstrings(strings, orbs, bitstring_type=BitstringType.INT)
-
         return convert_bitstring_type(
             strings,
             BitstringType.INT,
             bitstring_type,
             length=len(orbs),
         )
-
     else:
+        # Spinful case
         rdm_a, rdm_b = rdm
-
         n_a, n_b = nelec
         norb, _ = rdm_a.shape
-
         if orbs is None:
             orbs = (range(norb), range(norb))
         orbs_a, orbs_b = orbs
         orbs_a = cast(Sequence[int], orbs_a)
         orbs_b = cast(Sequence[int], orbs_b)
-
-        strings_a = _sample_spinless_direct(rdm_a, n_a, shots, rng)
-        strings_b = _sample_spinless_direct(rdm_b, n_b, shots, rng)
+        strings_a = _sample_slater_spinless(rdm_a, n_a, shots, rng)
+        strings_b = _sample_slater_spinless(rdm_b, n_b, shots, rng)
         strings_a = restrict_bitstrings(
             strings_a, orbs_a, bitstring_type=BitstringType.INT
         )
@@ -139,33 +133,38 @@ def sample_slater_determinant(
         )
 
 
-def _generate_marginals(
+def _sample_slater_spinless(
     rdm: np.ndarray,
-    sample: list[int],
-    empty_orbitals: list[int],
-) -> np.ndarray:
-    """Computes the marginal probabilities for adding a particle.
+    nelec: int,
+    shots: int,
+    seed: np.random.Generator | int | None = None,
+) -> list[int]:
+    """Collect samples of electronic configurations from a Slater determinant.
 
-    This is a step of the autoregressive sampling, and uses Bayes's rule.
+    The Slater determinant is defined by its one-body reduced density matrix (RDM).
+    The sampler uses a determinantal point process to auto-regressively produce
+    uncorrelated samples.
 
     Args:
-        rdm: A Numpy array with the one-body reduced density matrix.
-        pos_array: A Numpy array with the positions of the particles.
-        empty_orbitals: A Numpy array with the empty orbitals that a new particle
-            may occupy. The sorted union of ``pos_array`` and ``empty_orbitals``
-            must be equal to ``numpy.arange(num_orbitals)``.
+        rdm: The one-body reduced density matrix that defines the Slater determinant.
+        shots: Number of samples to collect.
+        seed: A seed to initialize the pseudorandom number generator.
+            Should be a valid input to ``np.random.default_rng``.
 
     Returns:
-        A Numpy array with the marginal corresponding to having the particles in the
-        position array and one extra in all possible empty orbitals.
-
+        The sampled bitstrings in integer format.
     """
-    marginals = np.zeros(len(empty_orbitals), dtype=float)
-    for i, orbital in enumerate(empty_orbitals):
-        new_sample = sample + [orbital]
-        rest_rdm = rdm[np.ix_(new_sample, new_sample)]
-        marginals[i] = np.linalg.det(rest_rdm).real
-    return marginals
+    norb, _ = rdm.shape
+
+    if nelec == 0:
+        return [0] * shots
+
+    if nelec == norb:
+        return [(1 << norb) - 1] * shots
+
+    rng = np.random.default_rng(seed)
+    samples = [_autoregressive_slater(rdm, norb, nelec, rng) for _ in range(shots)]
+    return [sum(1 << orb for orb in sample) for sample in samples]
 
 
 def _autoregressive_slater(
@@ -188,12 +187,13 @@ def _autoregressive_slater(
         A Numpy array with the position of the sampled electrons.
     """
     rng = np.random.default_rng(seed)
+    empty_orbitals = list(range(norb))
     probs = np.diag(rdm).real / nelec
-    sample = [rng.choice(norb, p=probs)]
-    marginal = [probs[sample[0]]]
-    all_orbs = set(range(norb))
-    empty_orbitals = list(all_orbs.difference(sample))
-    for k in range(nelec - 1):
+    index = rng.choice(len(empty_orbitals), p=probs)
+    sample = [empty_orbitals[index]]
+    marginal = [probs[index]]
+    empty_orbitals.pop(index)
+    for _ in range(nelec - 1):
         marginals = _generate_marginals(rdm, sample, empty_orbitals)
         conditionals = marginals / marginal[-1]
         conditionals /= np.sum(conditionals)
@@ -204,37 +204,25 @@ def _autoregressive_slater(
     return sample
 
 
-def _sample_spinless_direct(
-    rdm: np.ndarray,
-    nelec: int,
-    shots: int,
-    seed: np.random.Generator | int | None = None,
-) -> list[int]:
-    """Collect samples of electronic configurations from a Slater determinant for
-    spin-polarized systems.
+def _generate_marginals(
+    rdm: np.ndarray, sample: list[int], empty_orbitals: list[int]
+) -> np.ndarray:
+    """Computes the marginal probabilities for adding a particle.
 
-    The Slater determinant is defined by its one-body reduced density matrix (RDM).
-    The sampler uses a determinantal point process to auto-regressively produce
-    uncorrelated samples.
+    This is a step of the autoregressive sampling, and uses Bayes's rule.
 
     Args:
-        rdm: The one-body reduced density matrix that defines the Slater determinant
-            This is either a single Numpy array specifying the 1-RDM of a
-            spin-polarized system, or a pair of Numpy arrays where each element
-            of the pair contains the 1-RDM for each spin sector.
-        shots: Number of samples to collect.
-        seed: Either a Numpy random generator, an integer seed for the random number
-            generator or ``None``.
+        rdm: A Numpy array with the one-body reduced density matrix.
+        sample: The list of particle positions to add to.
+        empty_orbitals: The empty orbitals.
 
     Returns:
-        A 2D Numpy array with samples of electronic configurations.
-        Each row is a sample.
+        A Numpy array with the marginal corresponding to having the particles in the
+        position array and one extra in all possible empty orbitals.
     """
-    norb, _ = rdm.shape
-    if nelec == 0:
-        return [0] * shots
-    if nelec == norb:
-        return [(1 << norb) - 1] * shots
-    rng = np.random.default_rng(seed)
-    samples = [_autoregressive_slater(rdm, norb, nelec, rng) for _ in range(shots)]
-    return [sum(1 << orb for orb in sample) for sample in samples]
+    marginals = np.zeros(len(empty_orbitals), dtype=float)
+    for i, orbital in enumerate(empty_orbitals):
+        new_sample = sample + [orbital]
+        rest_rdm = rdm[np.ix_(new_sample, new_sample)]
+        marginals[i] = np.linalg.det(rest_rdm).real
+    return marginals
