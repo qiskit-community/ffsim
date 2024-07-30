5f18f2c23fd768612002ff8a95ce871fd0e7f428
haimeng-zhang <33587226+haimeng-zhang@users.noreply.github.com>
2024-07-30 15:18:04 -0400

    Add fswap gate (#295)
    * add fswap gate
    
    * remove line added by mistake
    
    * remove for loops in fswap test
    
    * fix fswap docstring
    
    * remove unused variable
    
    * shorten lines too long
    
    * update fswap gate docstring
    
    Co-authored-by: Kevin J. Sung <kevinjefferysung@gmail.com>
    
    * sampler test: use exact distribution for fidelity test
    
    * Direct slater sampler (#257)
    
    * add slater sampler
    
    * fix formatting
    
    * match signature of sample_state_vector
    
    * simplify tests
    
    * remove comments
    
    * consistent naming and copyright notice
    
    * test
    
    * suggested changes
    
    * added reference
    
    * implemented comments
    
    * suggested changes
    
    * small speedup
    
    * add copyright notice
    
    * delete unnecessary lines
    
    * remove variable not in use
    
    * refactor tests
    
    * redundant argument
    
    * rename function to sample_slater_determinant
    
    * test less
    
    * fix docstrings and minor refactoring
    
    * add test
    
    * remove unnecessary divisions
    
    * add restrict test
    
    ---------
    
    Co-authored-by: Kevin J. Sung <kevjsung@umich.edu>
    
    * remove unused variable
    
    * reformat
    
    * fix typo
    
    * update for consistent style
    
    Co-authored-by: Kevin J. Sung <kevinjefferysung@gmail.com>
    
    * fix indent and reformat
    
    ---------
    
    Co-authored-by: Haimeng Zhang <Haimeng.Zhang@ibm.com>
    Co-authored-by: Kevin J. Sung <kevinjefferysung@gmail.com>
    Co-authored-by: Kevin J. Sung <kevjsung@umich.edu>
    Co-authored-by: jrm874 <43007996+jrm874@users.noreply.github.com>

--- tests/python/gates/basic_gates_test.py
+++ tests/python/gates/basic_gates_test.py
@@ -595,3 +595,57 @@ def test_apply_fsim_gate_matrix_spinless(norb: int):
                     phase_11=phase_11,
                     norb=norb,
                 )
+
+
+@pytest.mark.parametrize("norb, spin", ffsim.testing.generate_norb_spin(range(4)))
+def test_apply_fswap_gate_matrix_spinful(norb: int, spin: ffsim.Spin):
+    """Test applying fSWAP gate matrix."""
+
+    mat01 = np.array([[0, 1], [1, 0]])
+
+    phase_00 = 1
+    phase_11 = -1
+
+    for i, j in itertools.combinations(range(norb), 2):
+        for target_orbs in [(i, j), (j, i)]:
+            assert_has_two_orbital_matrix(
+                lambda vec, norb, nelec: ffsim.apply_fswap_gate(
+                    vec,
+                    target_orbs=target_orbs,
+                    norb=norb,
+                    nelec=nelec,
+                    spin=spin,
+                ),
+                target_orbs=target_orbs,
+                mat=mat01,
+                phase_00=phase_00,
+                phase_11=phase_11,
+                norb=norb,
+                spin=spin,
+            )
+
+
+@pytest.mark.parametrize("norb", range(4))
+def test_apply_fswap_gate_matrix_spinless(norb: int):
+    """Test applying fSWAP gate matrix, spinless."""
+
+    mat01 = np.array([[0, 1], [1, 0]])
+
+    phase_00 = 1
+    phase_11 = -1
+
+    for i, j in itertools.combinations(range(norb), 2):
+        for target_orbs in [(i, j), (j, i)]:
+            assert_has_two_orbital_matrix_spinless(
+                lambda vec, norb, nelec: ffsim.apply_fswap_gate(
+                    vec,
+                    target_orbs=target_orbs,
+                    norb=norb,
+                    nelec=nelec,
+                ),
+                target_orbs=target_orbs,
+                mat=mat01,
+                phase_00=phase_00,
+                phase_11=phase_11,
+                norb=norb,
+            )
