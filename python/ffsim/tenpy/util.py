f53f6aaa19e3da3b0e048c90652fba164125cf53
bartandrews <bartandrews123@gmail.com>
2025-01-07 17:25:23 +0100

    map from ffsim to TeNPy ordering in bitstring_to_mps

--- python/ffsim/tenpy/util.py
+++ python/ffsim/tenpy/util.py
@@ -12,6 +12,7 @@
 
 from __future__ import annotations
 
+import tenpy.linalg.np_conserved as npc
 from tenpy.networks.mps import MPS
 from tenpy.networks.site import SpinHalfFermionSite
 
@@ -33,8 +34,8 @@ def bitstring_to_mps(bitstring: tuple[int, int], norb: int) -> MPS:
     string_b = format(int_b, f"0{norb}b")
 
     # relabel using TeNPy SpinHalfFermionSite convention
-    product_state = []
-    for site in zip(reversed(string_a), reversed(string_b)):
+    product_state, swap_factors = [], []
+    for i, site in enumerate(zip(reversed(string_a), reversed(string_b))):
         if site == ("0", "0"):
             product_state.append("empty")
         elif site == ("1", "0"):
@@ -44,8 +45,25 @@ def bitstring_to_mps(bitstring: tuple[int, int], norb: int) -> MPS:
         else:  # site == ("1", "1"):
             product_state.append("full")
 
+        if i > 0:
+            if product_state[-1] in ["up", "full"] and product_state[-2] in [
+                "down",
+                "full",
+            ]:
+                swap_factors.append(-1)
+            else:
+                swap_factors.append(1)
+
     # construct product state MPS
     shfs = SpinHalfFermionSite(cons_N="N", cons_Sz="Sz")
     mps = MPS.from_product_state([shfs] * norb, product_state)
+
+    # map from ffsim to TeNPy ordering
+    minus_identity_npc = npc.Array.from_ndarray(
+        -shfs.get_op("Id").to_ndarray(), [shfs.leg, shfs.leg.conj()], labels=["p", "p*"]
+    )
+    for i, swap_factor in enumerate(swap_factors):
+        if swap_factor == -1:
+            mps.apply_local_op(i, minus_identity_npc)
 
     return mps
