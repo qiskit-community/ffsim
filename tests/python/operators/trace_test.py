11d906f8acdb256e8d10f17e3f0084586cd4ce48
Minh Tran <minhtran@ibm.com>
2025-01-09 09:35:35 -0700

    addressing KS's comments

--- tests/python/operators/trace_test.py
+++ tests/python/operators/trace_test.py
@@ -1,13 +1,26 @@
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+
 import numpy as np
 import time
 import ffsim
 
-def test_trace(norb = 50, nelec = (3,3)):
-    n = norb
-    
-    h = np.matrix(np.random.rand(n,n))
+def test_trace():
+    norb = 50
+    nelec = (3,3)
+
+    rng = np.random.default_rng(12345)
+    h = np.matrix(rng.random((norb,norb)))
     h = h + h.H
-    V = np.array([np.matrix(np.random.rand(n,n)),np.matrix(np.random.rand(n,n))])
+    V = np.array([np.matrix(rng.random((norb,norb))),np.matrix(rng.random((norb,norb)))])
     H = ffsim.DiagonalCoulombHamiltonian(h,V)
     
     tic = time.time()
@@ -16,7 +29,7 @@ def test_trace(norb = 50, nelec = (3,3)):
     print(f"Trace = {t1}. Took {toc-tic}s by converting DiagonalCoulombHamiltonian to FermionOperator")
     
     tic = time.time()
-    t2 = ffsim.trace(H, norb=n, nelec=nelec)
+    t2 = ffsim.trace(H, norb=norb, nelec=nelec)
     toc = time.time()
     print(f"Trace = {t2}. Took {toc-tic}s using DiagonalCoulombHamiltonian._trace()")
 
