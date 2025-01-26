0fbe49f74ba9e868977eaee887560277f4dc9cfb
Minh C. Tran <32946896+minhctran@users.noreply.github.com>
2025-01-26 18:51:56 -0500

    Implement trace of FermionOperator (#349)
    * Fixed issue 348: implement trace of FermionOperator
    
    * addressing KS's comments
    
    * addressing KS's comment
    
    * addressing KS's comment
    
    * formatting
    
    * fix assignment type error
    
    * address KS's comments round 2
    
    * addressing KS's comments round 3
    
    * format
    
    * addressing KS's comments round 4
    
    * fix the phase bug for trace
    
    * added _trace_ to FermionOperator
    
    * revert trace_protocol back to before, added comments for rust
    
    * import binomial from num-integer
    
    * reformat fermion_operator_test.py
    
    * fix check
    
    * add test assertion and change test size
    
    * use
    
    ---------
    
    Co-authored-by: Kevin J. Sung <kevjsung@umich.edu>

--- tests/python/operators/fermion_operator_test.py
+++ tests/python/operators/fermion_operator_test.py
@@ -643,3 +643,22 @@ def test_mapping_methods():
         ((ffsim.cre_b(1), ffsim.des_b(2)), -0.5j),
         ((ffsim.cre_b(2), ffsim.des_b(1)), 1 - 0.5j),
     }
+
+
+def test_trace():
+    rng = np.random.default_rng(12345)
+    norb = 10
+    nelec = (3, 3)
+    ham: ffsim.DiagonalCoulombHamiltonian | ffsim.MolecularHamiltonian
+
+    # compare for random_diagonal_coulomb_hamiltonian
+    ham = ffsim.random.random_diagonal_coulomb_hamiltonian(norb, real=True, seed=rng)
+    t1 = ffsim.trace(ffsim.fermion_operator(ham), norb=norb, nelec=nelec)
+    t2 = ffsim.trace(ham, norb=norb, nelec=nelec)
+    np.testing.assert_allclose(t1, t2)
+
+    # compare for random_molecular_hamiltonian
+    ham = ffsim.random.random_molecular_hamiltonian(norb, seed=rng, dtype=float)
+    t1 = ffsim.trace(ffsim.fermion_operator(ham), norb=norb, nelec=nelec)
+    t2 = ffsim.trace(ham, norb=norb, nelec=nelec)
+    np.testing.assert_allclose(t1, t2)
