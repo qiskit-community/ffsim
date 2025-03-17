3bc8863aae5aa3b1f6b3a02682d0a26b617d44a2
Kevin J. Sung <kevjsung@umich.edu>
2025-03-17 11:31:18 -0400

    replace n_qubits in jordan-wigner with norb (#374)

--- tests/python/qiskit/jordan_wigner_test.py
+++ tests/python/qiskit/jordan_wigner_test.py
@@ -24,16 +24,16 @@ def test_random(norb: int, nelec: tuple[int, int]):
     op = ffsim.random.random_fermion_hamiltonian(norb, seed=rng)
     linop = ffsim.linear_operator(op, norb=norb, nelec=nelec)
     vec = ffsim.random.random_state_vector(ffsim.dim(norb, nelec), seed=rng)
+    expected_result = ffsim.qiskit.ffsim_vec_to_qiskit_vec(linop @ vec, norb, nelec)
 
     qubit_op = ffsim.qiskit.jordan_wigner(op)
     qubit_op_sparse = qubit_op.to_matrix(sparse=True)
     actual_result = qubit_op_sparse @ ffsim.qiskit.ffsim_vec_to_qiskit_vec(
         vec, norb, nelec
     )
-    expected_result = ffsim.qiskit.ffsim_vec_to_qiskit_vec(linop @ vec, norb, nelec)
     np.testing.assert_allclose(actual_result, expected_result, atol=1e-12)
 
-    qubit_op = ffsim.qiskit.jordan_wigner(op, n_qubits=2 * norb)
+    qubit_op = ffsim.qiskit.jordan_wigner(op, norb=norb)
     qubit_op_sparse = qubit_op.to_matrix(sparse=True)
     actual_result = qubit_op_sparse @ ffsim.qiskit.ffsim_vec_to_qiskit_vec(
         vec, norb, nelec
@@ -41,13 +41,36 @@ def test_random(norb: int, nelec: tuple[int, int]):
     np.testing.assert_allclose(actual_result, expected_result, atol=1e-12)
 
 
-def test_bad_qubit_number():
-    """Test passing bad number of qubits raises errors."""
+def test_more_norb():
+    """Test specifying more spatial orbitals than are present in the operator."""
+    op = ffsim.FermionOperator(
+        {
+            (ffsim.cre_a(1), ffsim.cre_b(1)): 1.0,
+            (ffsim.cre_a(2), ffsim.cre_b(2)): 1.0,
+        }
+    )
+    qubit_op = ffsim.qiskit.jordan_wigner(op, norb=5)
+    assert sorted(qubit_op.to_sparse_list()) == sorted(
+        [
+            ("YZZZZX", [1, 2, 3, 4, 5, 6], np.complex128(-0.25j)),
+            ("YZZZZY", [1, 2, 3, 4, 5, 6], np.complex128(-0.25 + 0j)),
+            ("XZZZZX", [1, 2, 3, 4, 5, 6], np.complex128(0.25 + 0j)),
+            ("XZZZZY", [1, 2, 3, 4, 5, 6], np.complex128(-0.25j)),
+            ("YZZZZX", [2, 3, 4, 5, 6, 7], np.complex128(-0.25j)),
+            ("YZZZZY", [2, 3, 4, 5, 6, 7], np.complex128(-0.25 + 0j)),
+            ("XZZZZX", [2, 3, 4, 5, 6, 7], np.complex128(0.25 + 0j)),
+            ("XZZZZY", [2, 3, 4, 5, 6, 7], np.complex128(-0.25j)),
+        ]
+    )
+
+
+def test_bad_norb():
+    """Test passing bad number of spatial orbitals raises errors."""
     op = ffsim.FermionOperator({(ffsim.cre_a(3),): 1.0})
     with pytest.raises(ValueError, match="non-negative"):
-        _ = ffsim.qiskit.jordan_wigner(op, n_qubits=-1)
-    with pytest.raises(ValueError, match="enough"):
-        _ = ffsim.qiskit.jordan_wigner(op, n_qubits=7)
+        _ = ffsim.qiskit.jordan_wigner(op, norb=-1)
+    with pytest.raises(ValueError, match="fewer"):
+        _ = ffsim.qiskit.jordan_wigner(op, norb=3)
 
 
 def test_hubbard():
