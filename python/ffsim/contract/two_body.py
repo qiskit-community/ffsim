bfb234136e32301c3049419a1b2a6e2ad4feef3f
Kevin J. Sung <kevjsung@umich.edu>
2025-09-13 21:18:17 -0400

    fix copyright year

--- python/ffsim/contract/two_body.py
+++ python/ffsim/contract/two_body.py
@@ -1,4 +1,4 @@
-# (C) Copyright IBM 2023.
+# (C) Copyright IBM 2025.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
