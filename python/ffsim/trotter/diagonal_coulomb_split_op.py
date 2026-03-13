2894f1c82cc2971a234e9d830fc5b41a2dded937
Kevin J. Sung <kevjsung@umich.edu>
2026-03-12 22:22:00 -0400

    don't apply constant if it's zero

--- python/ffsim/trotter/diagonal_coulomb_split_op.py
+++ python/ffsim/trotter/diagonal_coulomb_split_op.py
@@ -78,7 +78,8 @@ def simulate_trotter_diag_coulomb_split_op(
             order=order,
         )
     vec = apply_orbital_rotation(vec, current_basis, norb=norb, nelec=nelec, copy=False)
-    vec *= cmath.exp(-1j * time * hamiltonian.constant)
+    if hamiltonian.constant:
+        vec *= cmath.exp(-1j * time * hamiltonian.constant)
 
     return vec
 
