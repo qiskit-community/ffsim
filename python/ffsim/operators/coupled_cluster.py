587e72b9b827198af642dd7d51cf11a67e9705c9
Kevin J. Sung <kevjsung@umich.edu>
2026-04-08 15:58:07 -0400

    fix parentheses

--- python/ffsim/operators/coupled_cluster.py
+++ python/ffsim/operators/coupled_cluster.py
@@ -72,8 +72,8 @@ def singles_excitations_unrestricted(
     - :math:`a` runs over virtual spin-up orbitals,
     - :math:`I` runs over occupied spin-down orbitals,
     - :math:`A` runs over virtual spin-down orbitals,
-    - :math:`t^{(\alpha})_{ia}` are the spin-up singles amplitudes, and
-    - :math:`t^{(\beta})_{IA}` are the spin-down singles amplitudes.
+    - :math:`t^{(\alpha)}_{ia}` are the spin-up singles amplitudes, and
+    - :math:`t^{(\beta)}_{IA}` are the spin-down singles amplitudes.
 
     Args:
         t1: The singles amplitudes. This should be a pair of Numpy arrays,
@@ -163,12 +163,12 @@ def doubles_excitations_unrestricted(
     - :math:`a` and :math:`b` run over virtual spin-up orbitals,
     - :math:`I` and :math:`J` run over occupied spin-down orbitals,
     - :math:`A` and :math:`B` run over virtual spin-down orbitals,
-    - :math:`t^{(\alpha \alpha})_{ijab}` are the doubles amplitudes within spin-up
+    - :math:`t^{(\alpha \alpha)}_{ijab}` are the doubles amplitudes within spin-up
       orbitals,
-    - :math:`t^{(\alpha beta})_{iJaB}` are the doubles amplitudes between spin-up
-      and spin-down orbitals, and
-    - :math:`t^{(\beta \beta})_{IJAB}` are the doubles amplitudes within spin-down
-      orbitals.
+    - :math:`t^{(\beta \beta)}_{IJAB}` are the doubles amplitudes within spin-down
+      orbitals, and
+    - :math:`t^{(\alpha \beta)}_{iJaB}` are the doubles amplitudes between spin-up
+      and spin-down orbitals.
 
     Args:
         t2: The doubles amplitudes. This should be a tuple of three of Numpy arrays,
